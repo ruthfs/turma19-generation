b5ff6cc108e7eb99df0a37e3c054a28f3ab7becb
Your Name <you@example.com>
2021-03-24 17:44:33 -0300

    java1

---
+++ java/AULAS/src/Exemplo/OiMundo.java
@@ -0,0 +1,13 @@
+package Exemplo;
+
+public class OiMundo {
+
+		public static void main(String [] args) {
+			
+			//escreva
+			System.out.println("Olá Mundo!!");
+			System.out.println("Turma 19 Generation...");
+			System.out.println("Vamos programar em Java!!");
+			
+		}
+}
