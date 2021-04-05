c9e1a0004b46617622e5f956ef925658a3f9862e
Your Name <you@example.com>
2021-04-05 17:58:01 -0300

    aula 05/04

---
+++ java/AULAS/src/Exemplo/Invertervalores.java
@@ -0,0 +1,10 @@
+package Exemplo;
+
+public class Invertervalores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- java/AULAS/src/laco_de_decisao/Exercicio1.java
+++ java/AULAS/src/laco_de_decisao/Exercicio1.java
@@ -11,13 +11,13 @@ public class Exercicio1 {
 		
 		int num1=leitor.nextInt(), num2=leitor.nextInt(), num3=leitor.nextInt();
 		
-		if (num1>num2 && num1>num3) {
+		if (num1>=num2 && num1>=num3) {
 			System.out.println("O maior número é "+num1);
 		}
-		else if (num2>num1 && num2>num3) {
+		else if (num2>=num1 && num2>=num3) {
 			System.out.println("O maior número é "+num2);
 		}
-		else if (num3>num1 && num3>num2) {
+		else if (num3>=num1 && num3>=num2) {
 			System.out.println("O maior número é "+num3);
 		}
 		else {
